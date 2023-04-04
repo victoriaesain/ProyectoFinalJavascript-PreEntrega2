ebec5c83f2f30a840775d4249fd9d48c58f6aaa3
Victoria Esain <victoriaesain@hotmail.com>
2023-04-03 23:47:44 -0300

    cambios para la tercera pre-entrega

--- miarchivo.js
+++ miarchivo.js
@@ -1,86 +1,97 @@
-let listaProductos = [
-  { nombre: "Microfono", precio: 8000, id: 1 },
-  { nombre: "Bateria", precio: 120000, id: 2 },
-  { nombre: "Atril", precio: 4000, id: 3 },
-  { nombre: "Guitarra", precio: 20000, id: 4 },
-  { nombre: "Teclado", precio: 30000, id: 5 },
-  { nombre: "Flauta", precio: 6000, id: 6 },
-  { nombre: "Parlante", precio: 5000, id: 7 },
+const productos = [
+  { id: 1, nombre: "Microfono", precio: 6500 },
+  { id: 2, nombre: "Bateria", precio: 120000 },
+  { id: 3, nombre: "Atril", precio: 4300 },
+  { id: 4, nombre: "Guitarra", precio: 20000 },
+  { id: 5, nombre: "Teclado", precio: 30000 },
+  { id: 6, nombre: "Flauta", precio: 6000 },
+  { id: 7, nombre: "Violin", precio: 10000 },
+  { id: 8, nombre: "Trompeta", precio: 9500 },
+  { id: 9, nombre: "Ukelele", precio: 7500 },
 ];
 
-alert(
-  "Hola! gracias por visitar nuestra pagina, a continuacion te muestro nuestro catalogo de productos"
-);
-
-alert(
-  " Item 1: Microfono - Precio: $8000 \n Item 2: Bateria - Precio: $120000 \n Item 3: Atril - Precio: 4000 \n Item 4: Guitarra - Precio: $20000 \n Item 5: Teclado - Precio: $30000 \n Item 6: Flauta - Precio: $6000 \n Item 7: Parlante - Precio: $5000"
-);
+const listaProductos = document.getElementById("lista-productos");
 
 function mostrarProductos() {
-  for (const producto of productosSeleccionados) {
-    console.log(
-      "Item seleccionado: " + producto.nombre + " - precio: $" + producto.precio
-    );
+  listaProductos.innerHTML = "";
+  for (const producto of productos) {
+    const li = document.createElement("li");
+    li.innerHTML = `
+        <p>${producto.nombre} - $${producto.precio}</p>
+      `;
+    const boton = document.createElement("button");
+    boton.innerText = "Agregar al carrito";
+    boton.addEventListener("click", () => {
+      agregarAlCarrito(producto.id, producto.nombre, producto.precio);
+    });
+    li.append(boton);
+    listaProductos.append(li);
   }
 }
 
-let productosSeleccionados = [];
+const listaCarrito = document.getElementById("lista-carrito");
 
-function agregarProducto() {
-  let i = true;
-  while (i == true) {
-    let itemSeleccionado = parseInt(
-      prompt(
-        "Que producto desea adquirir? Indique el numero de item del producto a seleccionar: (0 para finalizar la compra)"
-      )
-    );
-    for (const item of listaProductos) {
-      if (item.id === itemSeleccionado) {
-        productosSeleccionados.push(item);
-      }
-    }
-    if (itemSeleccionado === 0) {
-      mostrarProductos();
-      i = false;
+const vaciarCarrito = document.getElementById("vaciar-carrito");
 
-      let formaDePago = parseInt(
-        prompt(
-          "Que medio de pago desea utilizar? (seleccione una opcion) \n 1: tarjeta de credito (3 cuotas sin interes) \n 2: tarjeta de credito (6 cuotas sin interes) \n 3: efectivo en la tienda (10% dcto)"
-        )
-      );
-      if (formaDePago === 1) {
-        alert(
-          "El total a abonar con tarjeta de credito es de: $" +
-            totalCompra() +
-            " (3 cuotas sin interes de $" +
-            parseInt(totalCompra() / 3) +
-            ")"
-        );
-      } else if (formaDePago === 2) {
-        alert(
-          "El total a abonar con tarjeta de credito es de: $" +
-            totalCompra() +
-            " (6 cuotas sin interes de $" +
-            parseInt(totalCompra() / 6) +
-            ")"
-        );
-      } else if (formaDePago === 3) {
-        alert(
-          "El total a abonar en efectivo (10% de descuento) es de: $" +
-            (totalCompra() - totalCompra() * 0.1)
-        );
-      }
-      alert("Muchas gracias por su compra!");
-    }
+let carrito = [];
+
+function agregarAlCarrito(id, nombre, precio) {
+  const producto = {
+    id: id,
+    nombre: nombre,
+    precio: precio,
+    cantidad: 1,
+  };
+  const productoExistente = carrito.find((p) => p.id === producto.id);
+  if (productoExistente) {
+    productoExistente.cantidad++;
+  } else {
+    carrito.push(producto);
   }
+  mostrarCarrito();
 }
 
-function totalCompra() {
-  let suma = 0;
-  for (const item of productosSeleccionados) {
-    suma += item.precio;
+function mostrarCarrito() {
+  listaCarrito.innerHTML = "";
+  for (const producto of carrito) {
+    const li = document.createElement("li");
+    li.innerHTML = `
+        <p>${producto.nombre} - $${producto.precio} x ${producto.cantidad}</p>
+      `;
+    const boton = document.createElement("button");
+    boton.innerText = "Eliminar";
+    boton.addEventListener("click", () => {
+      eliminarProducto(producto.id);
+    });
+    li.append(boton);
+    listaCarrito.append(li);
   }
-  return suma;
+  mostrarTotal();
 }
 
-agregarProducto();
+function eliminarProducto(id) {
+  carrito = carrito.filter((producto) => producto.id !== id);
+  mostrarCarrito();
+}
+
+vaciarCarrito.addEventListener("click", () => {
+  carrito = [];
+  mostrarCarrito();
+});
+
+function mostrarTotal() {
+  const totalCarrito = document.getElementById("total-carrito");
+  let total = 0;
+  for (let i = 0; i < carrito.length; i++) {
+    total += carrito[i].precio * carrito[i].cantidad;
+  }
+  totalCarrito.innerText = `$${total} \n\n OPCIONES DE PAGO: \n\nTarjeta de credito - 3 cuotas sin interes de: $${parseInt(
+    total / 3
+  )} \n\n Tarjeta de credito - 6 cuotas sin interes de: $${parseInt(
+    total / 6
+  )} \n\n Efectivo en la tienda (10% descuento): $${parseInt(
+    total - total * 0.1
+  )} `;
+}
+
+mostrarProductos();
